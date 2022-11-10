c301fa1663f6b15e1e4b17861f66e66ded011772
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-11-10 23:26:12 +0600

    try function add

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ app.use(cors());
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.a3ye7y2.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+        const serviceCollection = client.db('review').collection('services');
+        const reviewCollection = client.db ('review').collection('reviews');
+
+        app.get('/services', async(req, res) =>{
