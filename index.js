b25947888a24ac6d5b364d43e811e421e53981cd
sarkerjahiduzzaman <78559065+sarkerjahiduzzaman@users.noreply.github.com>
2022-11-10 23:28:20 +0600

    all data

--- index.js
+++ index.js
@@ -39,4 +39,26 @@ async function run(){
             res.send(result)
 
         }) 
+        app.get('/reviews', async(req, res) =>{
+            console.log(req.query.email);
+            let query ={};
+            if (req.query.email){
+                query={
+                    email: req.query.email
+                }
+            }
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
+        app.get('/alllreviews:/id', async(req, res) =>{
+        
+            const id =req.params.id;
+            const query={_id:ObjectId(id)}
+            
+            
+            const cursor = reviewCollection.find(query);
+            const reviews = await cursor.toArray();
+            res.send(reviews);
+        });
 
